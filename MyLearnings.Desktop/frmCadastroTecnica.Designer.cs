da6e1e2d3dd8833c9006a6e9f9b4a14af8c2d0ec
Tabata Mora <tabata.gabriela@hotmail.com>
2020-01-02 21:34:38 -0300

    Criando telas no form.

---
+++ MyLearnings.Desktop/frmCadastroTecnica.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace MyLearnings.Desktop
+{
+    partial class frmCadastroTecnica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastroTecnica));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.lblIdTenica = new System.Windows.Forms.Label();
+            this.lblNomeTecnica = new System.Windows.Forms.Label();
+            this.lblTCiclo = new System.Windows.Forms.Label();
+            this.lblDesCurto = new System.Windows.Forms.Label();
+            this.lblDescLongo = new System.Windows.Forms.Label();
+            this.lblDtCadastro = new System.Windows.Forms.Label();
+            this.lblIdUsuCadastro = new System.Windows.Forms.Label();
+            this.lblDtUsuAlteracao = new System.Windows.Forms.Label();
+            this.lblDtAlteracao = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chkPadrao = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox9);
+            this.panel1.Controls.Add(this.textBox8);
+            this.panel1.Controls.Add(this.textBox7);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.chkPadrao);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lblDtAlteracao);
+            this.panel1.Controls.Add(this.lblDtUsuAlteracao);
+            this.panel1.Controls.Add(this.lblIdUsuCadastro);
+            this.panel1.Controls.Add(this.lblDtCadastro);
+            this.panel1.Controls.Add(this.lblDescLongo);
+            this.panel1.Controls.Add(this.lblDesCurto);
+            this.panel1.Controls.Add(this.lblTCiclo);
+            this.panel1.Controls.Add(this.lblNomeTecnica);
+            this.panel1.Controls.Add(this.lblIdTenica);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Controls.Add(this.btnExcluir);
+            this.panel1.Controls.Add(this.btnAlterar);
+            this.panel1.Controls.Add(this.btnSalvar);
+            this.panel1.Controls.Add(this.btnInserir);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(540, 361);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.Location = new System.Drawing.Point(437, 268);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(80, 78);
+            this.btnCancelar.TabIndex = 17;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluir.Image")));
+            this.btnExcluir.Location = new System.Drawing.Point(332, 268);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(80, 78);
+            this.btnExcluir.TabIndex = 16;
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Image = ((System.Drawing.Image)(resources.GetObject("btnAlterar.Image")));
+            this.btnAlterar.Location = new System.Drawing.Point(227, 268);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(80, 78);
+            this.btnAlterar.TabIndex = 15;
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Image = ((System.Drawing.Image)(resources.GetObject("btnSalvar.Image")));
+            this.btnSalvar.Location = new System.Drawing.Point(123, 268);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(80, 78);
+            this.btnSalvar.TabIndex = 14;
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Image = ((System.Drawing.Image)(resources.GetObject("btnInserir.Image")));
+            this.btnInserir.Location = new System.Drawing.Point(21, 268);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(80, 78);
+            this.btnInserir.TabIndex = 13;
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // lblIdTenica
+            // 
+            this.lblIdTenica.AutoSize = true;
+            this.lblIdTenica.Location = new System.Drawing.Point(18, 16);
+            this.lblIdTenica.Name = "lblIdTenica";
+            this.lblIdTenica.Size = new System.Drawing.Size(16, 13);
+            this.lblIdTenica.TabIndex = 18;
+            this.lblIdTenica.Text = "Id";
+            // 
+            // lblNomeTecnica
+            // 
+            this.lblNomeTecnica.AutoSize = true;
+            this.lblNomeTecnica.Location = new System.Drawing.Point(18, 60);
+            this.lblNomeTecnica.Name = "lblNomeTecnica";
+            this.lblNomeTecnica.Size = new System.Drawing.Size(35, 13);
+            this.lblNomeTecnica.TabIndex = 19;
+            this.lblNomeTecnica.Text = "Nome";
+            // 
+            // lblTCiclo
+            // 
+            this.lblTCiclo.AutoSize = true;
+            this.lblTCiclo.Location = new System.Drawing.Point(20, 113);
+            this.lblTCiclo.Name = "lblTCiclo";
+            this.lblTCiclo.Size = new System.Drawing.Size(81, 13);
+            this.lblTCiclo.TabIndex = 21;
+            this.lblTCiclo.Text = "Tempo do Ciclo";
+            // 
+            // lblDesCurto
+            // 
+            this.lblDesCurto.AutoSize = true;
+            this.lblDesCurto.Location = new System.Drawing.Point(18, 159);
+            this.lblDesCurto.Name = "lblDesCurto";
+            this.lblDesCurto.Size = new System.Drawing.Size(119, 13);
+            this.lblDesCurto.TabIndex = 22;
+            this.lblDesCurto.Text = "Tempo Descanso Curto";
+            // 
+            // lblDescLongo
+            // 
+            this.lblDescLongo.AutoSize = true;
+            this.lblDescLongo.Location = new System.Drawing.Point(18, 206);
+            this.lblDescLongo.Name = "lblDescLongo";
+            this.lblDescLongo.Size = new System.Drawing.Size(124, 13);
+            this.lblDescLongo.TabIndex = 23;
+            this.lblDescLongo.Text = "Tempo Descanso Longo";
+            // 
+            // lblDtCadastro
+            // 
+            this.lblDtCadastro.AutoSize = true;
+            this.lblDtCadastro.Location = new System.Drawing.Point(297, 16);
+            this.lblDtCadastro.Name = "lblDtCadastro";
+            this.lblDtCadastro.Size = new System.Drawing.Size(90, 13);
+            this.lblDtCadastro.TabIndex = 24;
+            this.lblDtCadastro.Text = "Data do Cadastro";
+            // 
+            // lblIdUsuCadastro
+            // 
+            this.lblIdUsuCadastro.AutoSize = true;
+            this.lblIdUsuCadastro.Location = new System.Drawing.Point(403, 16);
+            this.lblIdUsuCadastro.Name = "lblIdUsuCadastro";
+            this.lblIdUsuCadastro.Size = new System.Drawing.Size(115, 13);
+            this.lblIdUsuCadastro.TabIndex = 25;
+            this.lblIdUsuCadastro.Text = "Id Usuário de Cadastro";
+            // 
+            // lblDtUsuAlteracao
+            // 
+            this.lblDtUsuAlteracao.AutoSize = true;
+            this.lblDtUsuAlteracao.Location = new System.Drawing.Point(204, 206);
+            this.lblDtUsuAlteracao.Name = "lblDtUsuAlteracao";
+            this.lblDtUsuAlteracao.Size = new System.Drawing.Size(118, 13);
+            this.lblDtUsuAlteracao.TabIndex = 26;
+            this.lblDtUsuAlteracao.Text = "Id Usuário de Alteração";
+            // 
+            // lblDtAlteracao
+            // 
+            this.lblDtAlteracao.AutoSize = true;
+            this.lblDtAlteracao.Location = new System.Drawing.Point(204, 159);
+            this.lblDtAlteracao.Name = "lblDtAlteracao";
+            this.lblDtAlteracao.Size = new System.Drawing.Size(93, 13);
+            this.lblDtAlteracao.TabIndex = 27;
+            this.lblDtAlteracao.Text = "Data de Alteração";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(62, 20);
+            this.textBox1.TabIndex = 28;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(496, 20);
+            this.textBox2.TabIndex = 29;
+            // 
+            // chkPadrao
+            // 
+            this.chkPadrao.AutoSize = true;
+            this.chkPadrao.Location = new System.Drawing.Point(153, 33);
+            this.chkPadrao.Name = "chkPadrao";
+            this.chkPadrao.Size = new System.Drawing.Size(60, 17);
+            this.chkPadrao.TabIndex = 30;
+            this.chkPadrao.Text = "Padrão";
+            this.chkPadrao.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(21, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(62, 20);
+            this.textBox3.TabIndex = 31;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(21, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(62, 20);
+            this.textBox4.TabIndex = 32;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(21, 223);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(62, 20);
+            this.textBox5.TabIndex = 33;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(300, 33);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 34;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(406, 33);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(62, 20);
+            this.textBox7.TabIndex = 35;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(207, 176);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 36;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(207, 223);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(62, 20);
+            this.textBox9.TabIndex = 37;
+            // 
+            // frmCadastroTecnica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 385);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmCadastroTecnica";
+            this.Text = "Cadastro da Técnica";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDtAlteracao;
+        private System.Windows.Forms.Label lblDtUsuAlteracao;
+        private System.Windows.Forms.Label lblIdUsuCadastro;
+        private System.Windows.Forms.Label lblDtCadastro;
+        private System.Windows.Forms.Label lblDescLongo;
+        private System.Windows.Forms.Label lblDesCurto;
+        private System.Windows.Forms.Label lblTCiclo;
+        private System.Windows.Forms.Label lblNomeTecnica;
+        private System.Windows.Forms.Label lblIdTenica;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox chkPadrao;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
--- MyLearnings.Desktop/frmLogin.Designer.cs
+++ MyLearnings.Desktop/frmLogin.Designer.cs
@@ -28,21 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.txtSenha);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Controls.Add(this.btnEntrar);
+            this.panel1.Controls.Add(this.lblSenha);
+            this.panel1.Controls.Add(this.lblUsuario);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(303, 288);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(96, 145);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(50, 13);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuário";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(96, 190);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(43, 13);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "Senha";
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(36, 232);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(103, 38);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "Efetuar Login";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(145, 232);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(103, 38);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(99, 206);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(98, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 111);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(327, 312);
+            this.Controls.Add(this.panel1);
             this.Name = "frmLogin";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblUsuario;
     }
 }
 
